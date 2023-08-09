438afbfb6b5c979d403d8b1f0ea7c721e8148c01
Stephen chukwuani <chukwuanisteven@gmail.com>
2023-08-09 23:31:49 +0100

    bug: cloudinary video not loading

--- app/(root)/explore/page.tsx
+++ app/(root)/explore/page.tsx
@@ -1,12 +1,8 @@
+"use client";
+
 import ExploreCard from "@/components/ExploreCard";
 
-const page = async () => {
-	const data = await fetch(
-		`https://api.unsplash.com/search/photos/?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}&query=street-photography,people`
-	).then((res) => res.json());
-
-	const RANDOM_IMAGES = data.results.map((item: any) => item.urls.regular);
-
+const page = () => {
 	return (
 		<main className="main-content">
 			<section className="discover-page">
--- components/ExploreCard.tsx
+++ components/ExploreCard.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+"use client";
 
 const ExploreCard = () => {
 	return (
